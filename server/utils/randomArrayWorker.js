6ef17a1300b7a25de8d31c22e73dd27d54233457
Sallam Rady <sallamrady@gmail.com>
2023-07-23 17:50:36 +0300

    fix random issue :)

--- server/utils/randomArrayWorker.js
+++ server/utils/randomArrayWorker.js
@@ -13,6 +13,7 @@ let counter = 0;
  */
 module.exports.getRandomList = function getRandomList(len = 10) {
   len = len > 15 ? 15 : len;
+  result = [];
   while (result.length < len) {
     let mod = counter % 4;
     switch (mod) {
@@ -31,42 +32,46 @@ module.exports.getRandomList = function getRandomList(len = 10) {
     }
     counter++;
   }
-  result = result.sort((a,b)=>(b.id * Math.random() - (a.id * Math.random())));
+  result = result.sort((a, b) => b.id * Math.random() - a.id * Math.random());
   return result;
 };
 
 function getNewVerb() {
   if (verbList.length) {
-    let index = Math.floor(Math.random() * verbList.length);
-    result.push(verbList[index]);
-    verbList.splice(index, 1);
+    let element = verbList[0];
+    result.push(element);
+    verbList.shift();
+    verbList.push(element);
   }
   return;
 }
 
 function getNewNoun() {
   if (nounList.length) {
-    let index = Math.floor(Math.random() * nounList.length);
-    result.push(nounList[index]);
-    nounList.splice(index, 1);
+    let element = nounList[0];
+    result.push(element);
+    nounList.shift();
+    nounList.push(element);
   }
   return;
 }
 
 function getNewAdjective() {
   if (adjectiveList.length) {
-    let index = Math.floor(Math.random() * adjectiveList.length);
-    result.push(adjectiveList[index]);
-    adjectiveList.splice(index, 1);
+    let element = adjectiveList[0];
+    result.push(element);
+    adjectiveList.shift();
+    adjectiveList.push(element);
   }
   return;
 }
 
 function getNewAdverb() {
   if (adverbList.length) {
-    let index = Math.floor(Math.random() * adverbList.length);
-    result.push(adverbList[index]);
-    adverbList.splice(index, 1);
+    let element = adverbList[0];
+    result.push(element);
+    adverbList.shift();
+    adverbList.push(element);
   }
   return;
 }
